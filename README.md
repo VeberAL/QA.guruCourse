4a15c4c2ea179da7bd71b2fba7ac65e0ad7b89a2
VeberAL <aleo83@rambler.ru>
2024-02-07 23:39:44 +0500

    Создать конфигурационный файл, заполнить ридми

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# QA.guru.02
+<h4> В этом проекте тестируется форма на сайте <a href="https://demoqa.com/automation-practice-form">DemoQA.com</a></h4>
+
+<h6>#Selenium      
+#JUnit</h6>
---
+++ src/test/java/TheJunitTests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TheJunitTests {
+
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(3>2);
+    }
+}
