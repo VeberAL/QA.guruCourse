8cb237142e6f4662daf43deca82131e04407416b
Alexander Veber <aleo83@rambler.ru>
2024-02-14 02:02:21 +0500

    Update findOnPageHomeWork.java
    Убрать строчку конфигурации

--- src/test/java/findOnPageHomeWork.java
+++ src/test/java/findOnPageHomeWork.java
@@ -12,7 +12,6 @@ public class findOnPageHomeWork {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy="eager";
-        Configuration.holdBrowserOpen = false;
     }
 
     @Test
