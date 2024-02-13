40c5ccda746c2ccdbf08b864b754eeb0a7fdd261
VeberAL <aleo83@rambler.ru>
2024-02-13 23:59:27 +0500

    Добавить тест по поиску на странице селенида в гитхаб в разделе wiki "SoftAssertions" пример кода для JUnit5

---
+++ src/test/java/findOnPageHomeWork.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class findOnPageHomeWork {
+    @BeforeAll
+    static void resolutionAndUrl() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy="eager";
+        Configuration.holdBrowserOpen = false;
+    }
+
+    @Test
+    void findJunitCodeForPage(){
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $$("ul.filterable-active div a").findBy(text("SoftAssertions")).click();
+        System.out.println("SoftAssertions найден в списке.");
+        $(".markdown-body").$(byText("Using JUnit5 extend test class:\n" +
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+        System.out.println("Пример кода для JUnit5 найден на странице.");
+    }
+}
