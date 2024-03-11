ce62b583bdae644f5abf0c524913c90cd5c52414
Alexander Veber <aleo83@rambler.ru>
2024-03-11 20:57:16 +0500

    Изменить описание step

--- src/test/java/tests/allure/Steps.java
+++ src/test/java/tests/allure/Steps.java
@@ -9,28 +9,28 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.By.linkText;
 
 public class Steps {
-    @Step("Откываем главную страницу сайта Github")
+    @Step("Открыть главную страницу сайта Github.")
     public void openMainPage() {
         open("https://www.github.com");
     }
 
-    @Step("Ищем репозиторий {repo} в поисковой строке сайта.")
+    @Step("Искать репозиторий {repo} в поисковой строке сайта.")
     public void searchForRepository(String repo) {
         $(".search-input").click();
         $("#query-builder-test").setValue(repo).submit();
     }
 
-    @Step("Кликаем по ссылке репозитория {repo}.")
+    @Step("Кликнуть по ссылке репозитория {repo}.")
     public void clickOnRepositoryLink(String repo) {
         $(linkText(repo)).click();
     }
 
-    @Step("Открываем вкладку Issues")
+    @Step("Открыть вкладку 'Issues'.")
     public void openIssueTab() {
         $("#issues-tab").click();
     }
 
-    @Step("Проверяем наличие Issues с номером #{issue}.")
+    @Step("Проверить наличие Issues с номером #{issue}.")
     public void shouldSeeIssueWithNumber(int issue) {
         $(withText("#" + issue)).should(Condition.exist);
     }
