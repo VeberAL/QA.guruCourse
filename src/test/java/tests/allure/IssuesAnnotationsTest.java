02da3afa37c61eddd41a057b8cfa29141d2a500b
VeberAL <aleo83@rambler.ru>
2024-03-11 20:52:12 +0500

    Добавить тесты с allure reports

--- src/test/java/tests/allure/IssuesAnnotationsTest.java
+++ src/test/java/tests/allure/IssuesAnnotationsTest.java
@@ -1,4 +1,27 @@
 package tests.allure;
 
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class IssuesAnnotationsTest extends BaseTest {
+    private static final String REPOSITORY = "VeberAL/QA.guruCourse";
+    private static final int ISSUE = 3;
+
+    @Test
+    @DisplayName("Проверка созданного Issue #" + ISSUE + " в проекте " + REPOSITORY)
+    @Feature("Issue в репозитории.")
+    @Story("Проверка Issue.")
+    @Owner("Вебер А.Л.")
+    public void githubIssuesAnnotatedStepTest() {
+        Steps steps = new Steps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssueTab();
+        steps.shouldSeeIssueWithNumber(ISSUE);
+    }
 }
--- src/test/java/tests/allure/IssuesLambdaTest.java
+++ src/test/java/tests/allure/IssuesLambdaTest.java
@@ -1,4 +1,31 @@
 package tests.allure;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
 public class IssuesLambdaTest extends BaseTest {
+    private static final String REPOSITORY = "VeberAL/QA.guruCourse";
+    private static final int ISSUE = 3;
+
+    @Test
+    public void githubIssuesStepsTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открыть главную страницу сайта Github", () -> open("https://www.github.com"));
+        step("Искать репозиторий " + REPOSITORY + " в поисковой строке сайта.", () -> {
+            $(".search-input").click();
+            $("#query-builder-test").setValue(REPOSITORY).submit();
+        });
+        step("Кликнуть по ссылке репозитория" + REPOSITORY + ".", () -> $(linkText(REPOSITORY)).click());
+        step("Открыть вкладку 'Issues'", () -> $("#issues-tab").click());
+        step("Проверить наличие Issues с номером #" + ISSUE + ".", () -> $(withText("#" + ISSUE)).should(Condition.exist));
+    }
 }
