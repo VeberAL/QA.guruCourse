a61b96cbf749703f041ec1d71cbbabbcf6644a20
VeberAL <aleo83@rambler.ru>
2024-03-19 00:46:19 +0500

    Добавить параметры

--- src/test/java/tests/remotetest/TestBase.java
+++ src/test/java/tests/remotetest/TestBase.java
@@ -6,6 +6,7 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -34,6 +35,11 @@ public class TestBase {
         ));
         Configuration.browserCapabilities = capabilities;
 
+
+    }
+
+    @BeforeEach
+    void beforeEach() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
